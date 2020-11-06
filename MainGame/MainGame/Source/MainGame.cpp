d5365dc7fa346fb156ca0781ca3a2d8c396cbd79
p.p.p <toide213@gmail.com>
2020-11-06 17:10:25 +0900

    Mainの実装

--- MainGame/MainGame/Source/MainGame.cpp
+++ MainGame/MainGame/Source/MainGame.cpp
@@ -1,7 +1,35 @@
 ﻿#include "MainGame/Include/Application.hpp"
+#include <stdlib.h>
+#include <iostream>
+#include <Windows.h>
 
+
+#if _DEBUG
 int main()
+#else
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCommandLine, _In_ int nShowCommand)
+#endif
 {
-    return 0;
+    /********************************************
+    **         Initialize
+    *********************************************/
+    Application& application = Application::Instance();
+    if (!application.Initialize())
+    {
+        std::cout << "Initialize Error" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    /********************************************
+    **         MainLoop
+    *********************************************/
+    application.Run();
+
+    /********************************************
+    **         Termination Process
+    *********************************************/
+    application.Terminate();
+    exit(EXIT_SUCCESS);
+
 }
 
