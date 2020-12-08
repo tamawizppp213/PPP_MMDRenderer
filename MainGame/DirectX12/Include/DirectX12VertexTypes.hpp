56539a1b6b478898a8d457f4d7052de177b2e95e
p.p.p <toide213@gmail.com>
2020-12-09 04:22:10 +0900

    コメント

--- MainGame/DirectX12/Include/DirectX12VertexTypes.hpp
+++ MainGame/DirectX12/Include/DirectX12VertexTypes.hpp
@@ -1,3 +1,9 @@
+//////////////////////////////////////////////////////////////////////////////////
+///             @file   DirectX12VertexTypes.hpp
+///             @brief  DirectX12 Vertex Types
+///             @author Toide Yutaro (Reference DirectXTK)
+///             @date   2020_11_19
+//////////////////////////////////////////////////////////////////////////////////
 #pragma once
 #ifndef DIRECTX12VERTEXTYPES_HPP
 #define DIRECTX12VERTEXTYPES_HPP
